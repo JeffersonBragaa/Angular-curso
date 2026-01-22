a2216c9e88f8ac15d05bde5d664fd7b42d399644
Jefferson Braga <Jekonth@gmail.com>
2026-01-22 16:01:47 -0300

    continuação, aula atual 19

---
+++ modulo-01/src/app/lista-compras/lista-compras.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-lista-compras',
+  imports: [FormsModule],
+  templateUrl: './lista-compras.component.html',
+  styleUrl: './lista-compras.component.scss'
+})
+export class ListaComprasComponent {
+  item: string = '';
+
+  adicionarItem() {
+    console.log('Item adicionado:', this.item);
+    this.item = '';
+  }
+}
