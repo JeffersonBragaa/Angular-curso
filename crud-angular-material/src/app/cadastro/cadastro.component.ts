074119cbb73d1982537b56b669c46b09d1ee971d
Jefferson Braga <Jekonth@gmail.com>
2026-01-27 15:54:05 -0300

    ep 30 modulo-02

--- crud-angular-material/src/app/cadastro/cadastro.component.ts
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -4,6 +4,10 @@ import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import { MatFormFieldModule} from "@angular/material/form-field"; 
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
 
 
 
@@ -13,11 +17,21 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule, 
     FormsModule, 
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule, 
+    MatIconModule, 
+    MatButtonModule
   ],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss'
 })
 export class CadastroComponent {
+  cliente: Cliente = Cliente.newCliente();
 
+  constructor(private service: ClienteService){
+
+  }
+
+  salvar(){
+    this.service.salvar(this.cliente);
+  }
 }
